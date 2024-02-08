aeea464963c722268b263c152b7e4df1e01897af
Jobayer98 <joobayerrahman0220@gmail.com>
2024-02-08 15:40:56 +0600

    add employee and device serializer

--- assets_tracking_api/serializers.py
+++ assets_tracking_api/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
-from .models import Company
+from django.contrib.auth.models import User
+from .models import Company, Device, Employee
 
-
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+        
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
         fields = '__all__'
+        
+class DeviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Device
+        fields = '__all__'
+        
+        
+class EmployeeSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    company = CompanySerializer(read_only=True)
+    user_id = serializers.IntegerField(write_only=True)
+    company_id = serializers.IntegerField(write_only=True)
+    class Meta:
+        model = Employee
+        fields = ['id', 'address', 'designation', 'company', 'user', 'user_id', 'company_id']
