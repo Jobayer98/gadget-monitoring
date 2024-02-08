33f01402bdd0ab1c041dc7044d2eab07ff6d9a6c
Jobayer98 <joobayerrahman0220@gmail.com>
2024-02-08 15:05:42 +0600

    write company view

--- assets_tracking_api/views.py
+++ assets_tracking_api/views.py
@@ -1,6 +1,49 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from .serializers import CompanySerializer
+from .models import Company
 
 @api_view()
 def index(request):
     return Response("Hello world")
+
+class CompanyList(APIView):
+    def get(self, request):
+        company = Company.objects.all()
+        serializer = CompanySerializer(company, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CompanySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+
+class CompanyDetail(APIView):
+    def get(self, request, pk):
+        company = get_object_or_404(Company, pk=pk)
+        serializer = CompanySerializer(company)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        company = get_object_or_404(Company, pk=pk)
+        serializer = CompanySerializer(company, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def patch(self, request, pk):
+        company = get_object_or_404(Company, pk=pk)
+        serializer = CompanySerializer(company, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def delete(self, request, pk):
+        company = get_object_or_404(Company, pk=pk)
+        company.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
