f29a5beb0875c0796f2e72e955b81f781e778260
Jobayer98 <joobayerrahman0220@gmail.com>
2024-02-08 15:41:28 +0600

    add view for Employee

--- assets_tracking_api/urls.py
+++ assets_tracking_api/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('company/', views.CompanyList.as_view(), name='company_list'),
-    path('company/<int:pk>', views.CompanyDetail.as_view(), name='company_detail'),
+    path('company/<int:pk>/', views.CompanyDetail.as_view(), name='company_detail'),
+    path('company/employees/', views.EmployeeList.as_view(), name='employee_list'),
+    path('company/employees/<int:pk>/', views.EmployeeDetail.as_view(), name='employee_detail'),
 ]
--- assets_tracking_api/views.py
+++ assets_tracking_api/views.py
@@ -2,9 +2,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
-from .serializers import CompanySerializer
-from .models import Company
+from .serializers import CompanySerializer, EmployeeSerializer
+from .models import Company, Employee
 
 @api_view()
 def index(request):
@@ -46,4 +47,44 @@ class CompanyDetail(APIView):
     def delete(self, request, pk):
         company = get_object_or_404(Company, pk=pk)
         company.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    
+class EmployeeList(APIView):
+    def get(self, request):
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data)
+    
+    @csrf_exempt
+    def post(self, request):
+        serializer = EmployeeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class EmployeeDetail(APIView):
+    def get(self, request, pk):
+        employee = get_object_or_404(Employee, pk=pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        employee = get_object_or_404(Employee, pk=pk)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def patch(self, request, pk):
+        employee = get_object_or_404(Employee, pk=pk)
+        serializer = EmployeeSerializer(employee, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def delete(self, request, pk):
+        employee = get_object_or_404(Employee, pk=pk)
+        employee.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
