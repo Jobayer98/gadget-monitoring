86718ab40c79a325dd3a42088072a4da52a919e7
Jobayer98 <joobayerrahman0220@gmail.com>
2024-02-08 22:28:25 +0600

    add gitignore

--- assets_tracking_api/urls.py
+++ assets_tracking_api/urls.py
@@ -6,12 +6,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Your API",
+        title="Device tracking api",
         default_version='v1',
-        description="Your API description",
-        terms_of_service="https://www.yourapp.com/terms/",
-        contact=openapi.Contact(email="contact@yourapp.com"),
-        license=openapi.License(name="Your License"),
+        description="Django app to track corporate assets such as phones, tablets laptops and other gears handed out to employees."
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
