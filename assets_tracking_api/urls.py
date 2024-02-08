b37f5e589030386df06350518bc7847311c01e85
Jobayer98 <joobayerrahman0220@gmail.com>
2024-02-08 15:06:18 +0600

    add company serializer

--- assets_tracking_api/urls.py
+++ assets_tracking_api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('company/', views.CompanyList.as_view(), name='company_list'),
+    path('company/<int:pk>', views.CompanyDetail.as_view(), name='company_detail'),
 ]
